7e639fa15f4b45a7b9df7d5a7a6251a9459ab7bb
Mattias Erming <mattias@mattiaserming.com>
2014-03-31 21:14:17 +0200

    Implemented tab completion

--- jquery.tabComplete.js
+++ jquery.tabComplete.js
@@ -9,11 +9,43 @@
  */
 
 (function($) {
-	$.fn.tabComplete = function(values) {
-		if (this.size() > 1) {
-			return this.each(function() {
-				$(this).tabComplete(values);
+	$.fn.tabComplete = function(list) {
+		var self = this;
+		if (self.size() > 1) {
+			return self.each(function() {
+				$(this).tabComplete(list);
 			});
 		}
+		
+		var match = [];
+		self.on('keydown', function(e) {
+			var key = e.which;
+			if (key != 9) {
+				match = [];
+				return;
+			}
+			
+			var text = self.val().split(' ');
+			var last = text.splice(-1)[0];
+			
+			if (!match.length) {
+				match = $.grep(list, function(w) {
+					return last != '' && w.indexOf(last) !== -1;
+				});
+			}
+			
+			var i = match.indexOf(last) + 1;
+			if (i == match.length) {
+				i = 0;
+			}
+			
+			if (match.length) {
+				last = match[i];
+			}
+			
+			text.push(last);
+			self.val(text.join(' '));
+			return false;
+		});
 	};
 })(jQuery);
