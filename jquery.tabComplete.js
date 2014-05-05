9aa1db2dc5c44b664be6a6ebd09d7a7e3d528238
Mattias Erming <mattias@mattiaserming.com>
2014-05-05 23:34:26 +0200

    Boilerplate code

--- jquery.tabComplete.js
+++ jquery.tabComplete.js
@@ -8,70 +8,19 @@
  * Version 0.2.4
  */
 (function($) {
-	$.fn.tabComplete = function(options) {
-		var settings = $.extend({
-			after: '',
-			caseSensitive: false,
-			list: [],
+	$.fn.tabComplete = function(words, options) {
+		return this.each(function() {
+			tabComplete.call(this, words, options);
+		});
+	};
+	
+	function tabComplete(words, options) {
+		options = $.extend({
+			// ..
 		}, options);
 		
-		var self = this;
-		if (self.size() > 1) {
-			return self.each(function() {
-				$(this).tabComplete(options);
-			});
-		}
-		
-		// Keep the list stored in the DOM via jQuery.data()
-		self.data('list', settings.list);
-		
-		var match = [];
-		self.on('keydown', function(e) {
-			var key = e.which;
-			if (key != 9) {
-				match = [];
-				return;
-			}
-			
-			var text = self.val().trim().split(' ');
-			var last = text.splice(-1)[0];
-			
-			if (!match.length) {
-				match = [];
-				$.each(self.data('list'), function(i, w) {
-					var l = last;
-					if (l == '') {
-						return;
-					} else if (typeof w === "function") {
-						var words = w(l);
-						if (words) {
-							match = match.concat(words);
-						}
-					} else if (!settings.caseSensitive) {
-						if (0 == w.toLowerCase().indexOf(l.toLowerCase())) {
-							match.push(w);
-						}
-					} else {
-						if (0 ==  w.indexOf(l)) {
-							match.push(w);
-						}
-					}
-				});
-			}
-			
-			var i = match.indexOf(last) + 1;
-			if (i == match.length) {
-				i = 0;
-			}
-			
-			if (match.length) {
-				text.push(match[i]);
-				self.val(text.join(' ') + settings.after);
-			}
-			
-			return false;
+		$(this).on("keydown.tabComplete", function(e) {
+			console.log(e.which);
 		});
-		
-		return this;
-	};
+	}
 })(jQuery);
