abbf8e03c205133ff1423c1b508796d1469fbc5c
Mattias Erming <mattias@mattiaserming.com>
2014-05-13 23:18:43 +0200

    Release 1.0.0-alpha2

--- jquery.tabComplete.js
+++ jquery.tabComplete.js
@@ -5,7 +5,7 @@
  * Copyright (c) 2014 Mattias Erming <mattias@mattiaserming.com>
  * Licensed under the MIT License.
  *
- * Version 1.0.0-alpha1
+ * Version 1.0.0-alpha2
  */
 (function($) {
 	var defaults = {
